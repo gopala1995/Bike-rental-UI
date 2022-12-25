b68818a19ee5b6a1ef79b3fd050094d1701faea6
Gopala <guptaraja727@gmail.com>
2022-12-25 10:15:56 +0530

    commitr

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import { AppBar,Container,Typography,IconButton } from '@mui/material'
+import MenuIcon from '@mui/icons-material/Menu';
 
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div>
+    <AppBar position="static">
+       <Container maxWidth="xl">
+       <IconButton
+            size="large"
+            edge="start"
+            color="black"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+        <Typography>About</Typography>
+       </Container>
+    </AppBar>
+    </div>
   )
 }
