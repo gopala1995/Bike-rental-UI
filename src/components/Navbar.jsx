d4ae52d8df54d769c6aad47c474f62103c353344
Gopala <guptaraja727@gmail.com>
2022-12-26 15:25:18 +0530

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,16 @@
 import React from "react";
-import { AppBar, Toolbar, Typography, IconButton, styled } from "@mui/material";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  IconButton,
+  styled,
+  Button,
+} from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
-import {Select} from "@mui/material";
-import {MenuItem} from "@mui/material";
-import { Image } from "@mui/icons-material";
+import { Select } from "@mui/material";
+import { MenuItem } from "@mui/material";
+import LocationOnOutlinedIcon from "@mui/icons-material/LocationOnOutlined";
 
 const TopTypo = styled(Typography)`
   background-color: #fed250;
@@ -16,9 +23,24 @@ const HedBar = styled(AppBar)`
   position: static;
 `;
 
+const ToolB = styled(Toolbar)`
+  margin: 10px;
+`;
 const TritBar = styled(Typography)`
- color: #303030;
-`
+  color: #303030;
+  margin-left: 40px;
+`;
+const SelectIOption = styled(Select)`
+  width: 150px;
+  height: 35px;
+  margin-left: 25px;
+  border: 0px;
+`;
+
+const Btn = styled(Button)`
+  background-color: #fed250;
+  color: black;
+`;
 
 export const Navbar = () => {
   return (
@@ -28,28 +50,38 @@ export const Navbar = () => {
         <span style={{ color: "blue", marginLeft: "5px" }}>Know more</span>
       </TopTypo>
       <HedBar>
-        <Toolbar>
+        <ToolB>
           <IconButton>
             <MenuIcon />
           </IconButton>
-          <Image src={logo192.png}/>
+          <img src={"logo1.png"} style={{ width: "150px", height: "50px" }} />
+          <img
+            src={"logo2.png"}
+            style={{ width: "70px", height: "40px", marginLeft: "60px" }}
+          />
           <TritBar>Tariff</TritBar>
-          <Select>
-          <MenuItem  disabled value="">
-            <em>What's new?</em>
-          </MenuItem>
-          <MenuItem>store</MenuItem>
-          <MenuItem>Inter-city travels</MenuItem>
-          </Select>
+          <SelectIOption>
+            <MenuItem>What's new?</MenuItem>
+            <MenuItem>store</MenuItem>
+            <MenuItem>Inter-city travels</MenuItem>
+          </SelectIOption>
           <TritBar>Offer</TritBar>
-          <Select>
-          <MenuItem disabled value="">
-            <em>Partnet with us</em>
-          </MenuItem>
-          <MenuItem>Earn with us</MenuItem>
-          <MenuItem>Own a franchise</MenuItem>
-          </Select>
-        </Toolbar>
+          <SelectIOption>
+            <MenuItem>Partner with us</MenuItem>
+            <MenuItem>Earn with us</MenuItem>
+            <MenuItem>Own a franchise</MenuItem>
+          </SelectIOption>
+          <SelectIOption
+            style={{ marginLeft: "80px", border: "1px solid  #fed250" }}
+          >
+            <MenuItem>
+              <LocationOnOutlinedIcon />
+              Banglore
+            </MenuItem>
+          </SelectIOption>
+          <Button style={{ color: "black", marginLeft: "50px" }}>Login</Button>
+          <Btn style={{ marginLeft: "50px" }}>Signup</Btn>
+        </ToolB>
       </HedBar>
     </div>
   );
